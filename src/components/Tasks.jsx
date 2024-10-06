417ba0d0015bb8e476f51d96eb6c040d22f13e01
Poornima Kumawat <89656087+CodingShinchan@users.noreply.github.com>
2024-10-06 13:27:39 +0530

    Update Tasks.jsx

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -20,7 +20,7 @@ const Tasks = () => {
     const fetchTasks = async () => {
       setLoading(true);
       try {
-        const response = await axios.get("https://back-up-office-panel-1hlc.vercel.app/tasklists");
+        const response = await axios.get("https://backup-office-panel.onrender.com/tasklists");
         const taskData = [];
 
         response.data.forEach((user) => {
